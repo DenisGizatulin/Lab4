68b9ec9cc750690bc99d369a1a312a80ec1ca2e8
DenisGizatulin <denis_gizatulin@outlook.com>
2024-12-18 16:54:13 +0300

    added people_in_space func

--- main.py
+++ main.py
@@ -31,15 +31,39 @@ def get_photo_of_the_day(message):
         bot.send_message(message.chat.id, "Не удалось получить данные о фотографии дня.")
 
 
+def people_in_space(message):
+    url = 'http://api.open-notify.org/astros.json'
+    res = requests.get(url)
+
+    if res.status_code == 200:
+        data = res.json()
+        people = data['people']
+
+        # Формируем сообщение с информацией о людях в космосе
+        response_text = "В космосе сейчас находятся следующие астронавты:\n\n"
+
+        for i, person in enumerate(people, start=1):  # Используем enumerate для индексации
+            response_text += f"{i}. {person['name']} на {'МКС' if person['craft'] == 'ISS' else 'Тяньгун'}\n"
+
+        # Отправляем сообщение
+        bot.send_message(message.chat.id, response_text)
+    else:
+        bot.send_message(message.chat.id, "Не удалось получить данные о людях в космосе.")
+
+
 def menu(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('Космическое фото дня', callback_data='get_photo'))
-    bot.send_message(message.chat.id, 'Меню', reply_markup=markup)
+    markup.add(types.InlineKeyboardButton('Сколько людей сейчас в космосе', callback_data='people_in_space'))
+    bot.send_message(message.chat.id, 'Меню: ', reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data == 'get_photo')
+@bot.callback_query_handler(func=lambda call: call.data in ['get_photo', 'people_in_space'])
 def handle_query(call):
-    get_photo_of_the_day(call.message)
+    if call.data == 'get_photo':
+        get_photo_of_the_day(call.message)
+    elif call.data == 'people_in_space':
+        people_in_space(call.message)
 
 
 if __name__ == '__main__':
